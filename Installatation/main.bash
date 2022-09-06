9cc123e059adc80a45a08533690c1af3937b0c3c
osf <ashok_a380@icloud.com>
2022-09-06 12:33:14 +0530

    update keys for ROS install

--- Installatation/main.bash
+++ Installatation/main.bash
@@ -14,5 +14,10 @@ curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 if [[ $(uname -m) == "aarch64" ]]; then
     sudo apt-get install rpi.gpio
 fi
-chmod a+x Installatation/ros2.bash
-sudo sh Installatation/ros2.bash
+
+git clone https://gist.github.com/7101c20b7055d68e6f57b34687e4b8cd.git
+
+mv 7101c20b7055d68e6f57b34687e4b8cd ros2_install
+
+chmod a+x ros2_install/ROS2_FOXY_HUMBLE_INSTALL.bash
+bash ros2_install/ROS2_FOXY_HUMBLE_INSTALL.bash
